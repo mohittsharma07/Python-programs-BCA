20b3d03d5baa7a54a4f4db321be8954238c47a64
mohittsharma07 <mohittt247@gmail.com>
2025-06-27 01:50:19 +0530

    first Python program with variables
    A simple program that prints my name, age, college with variables

--- first_program.py
+++ first_program.py
@@ -1,2 +1,10 @@
-print("Hello, my name is Mohit Sharma")
-print("Welcome to Python programming")
+# print("Hello, my name is Mohit Sharma")
+# print("Welcome to Python programming")
+
+name = "Mohit Sharma"
+age = 17
+college = "CSJMU kanpur"
+
+print(name)
+print(age)
+print(college)
