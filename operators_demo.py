0e3323a1a2b234b5d495a255898d512cca60072f
mohittsharma07 <mohittt247@gmail.com>
2025-06-27 03:20:05 +0530

    Added demo of all basic python operators
    This Python file demonstrates various types of operators:
    - Arithmetic operators (+, -, *, /, %, **)
    - Relational/comparison operators (==, !=, >, <, >=, <=)
    - Assignment operators (=, +=, -=, *=, /=)
    - Logical operators (and, or, not)
    
    It prints the results for different combinations of values to help understand how these operators work in Python.

---
+++ operators_demo.py
@@ -0,0 +1,64 @@
+# Arithmetic Operators
+
+a = 5
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a ** b) #a^b
+print(a % b) #remainder
+
+
+# Relational operators / comparison operator
+
+a = 100
+b = 40
+
+print(a == b )
+print(a !=b )
+print(a >= b )
+print(a > b )
+print(a <= b )
+print(a < b )
+
+
+# Assignment operators
+
+num = 30
+num += 20
+
+print("num :", num)
+ 
+num = 100
+num *= 100
+
+print("num :", num)
+ 
+num = 10
+num /= 5
+print("num :", num)
+
+num = 10
+num %= 5
+print("num :", num)
+
+num = 20
+num **= 5
+print("num :", num)
+
+
+# Logical operators
+
+a = 60 
+b = 10
+print(not False)
+print(not (a > b))
+
+
+val1 = False
+val2 = False 
+print("AND operator :", val1 and val2)
+
+print("OR operator :", (a == b) or (a > b))
