21c126c8055fc9fa2f425c553268ed30fb3e0922
mohittsharma07 <mohittt247@gmail.com>
2025-06-30 22:03:05 +0530

    Add basic input and math operation programs
    This file contains four basic Python programs :-
    1. Input two integers and print their sum.
    2. Input the side of a square and print its area.
    3. Input two floating-point numbers and print their average.
    4. Compare two numbers and print if the first is greater than the second.

---
+++ basic_input_math_operations.py
@@ -0,0 +1,26 @@
+# Write a program to input 2 numbers and print sum.
+
+first = int(input("Enter first :"))
+second = int(input("Enter second :"))
+
+print("sum = ", first + second )
+
+# Write a program to input side of a square & print its area.
+
+side = float(input("Enter square side :"))
+
+print("area = ",side ** 2)
+
+# Write a program to input 2 floating point numbers & print their average.
+
+a = float(input("Enter first :"))
+b = float(input("Enter second :"))
+
+print(" avg = ", (a+b)/2)
+
+# Write a program to input 2 int numbers, a and b and print true if a is greater than or equal to b.
+
+a = int(input("Enter first :"))
+b = float(input("Enter second :"))
+
+print(a >= b)
