67bf7b36c7a53873dab857a7e781332c646c57ac
mohittsharma07 <mohittt247@gmail.com>
2025-07-01 16:47:20 +0530

    Add string indexing ex. with positive and negative
    This program demonstrates simple string indexing in python using both positive and negative indices. Useful for beginners learning about how to access character in a string.

---
+++ string_indeixng_example.py
@@ -0,0 +1,16 @@
+# Python Indexing Example:-
+
+text = "Python"
+
+# Indexing (positive)
+print("First character:", text[0])
+print("Third character:", text[2])
+
+# Indexing (negative)
+print("Last character:", text[-1])
+print("Second last character:",text[-2])
+      
+
+
+
+    
