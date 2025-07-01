2b6445c51accee2840642d0ed613167fc7f4edb1
mohittsharma07 <mohittt247@gmail.com>
2025-07-01 17:24:30 +0530

    Add demo of common python string functions
    This Python program demonstrates common string functions such as endswith(), capitalize(), replace(), find(), and count() using a simple example string. Helpful for beginners learning Python string methods.

---
+++ string_functions_demo.py
@@ -0,0 +1,23 @@
+# str.endswith("er") :- 
+str = "I am studying python"
+print(str.endswith("ege"))
+
+# str.capitalize() :-
+str = "i am studying python"
+print(str.capitalize())
+
+# str.replace(old,new) :-
+str = "i am studying python"
+print(str.replace("o","a"))
+
+# str.find(word) :-
+str = "i am studying python"
+print(str.find("o"))
+
+# str.count("am") :-
+str = "i am studying python"
+print(str.count("am"))
+
+
+
+
