efe56475cfeaf97aa686219242d59beedbd20dc8
mohittsharma07 <mohittt247@gmail.com>
2025-07-01 17:02:29 +0530

    Add string slicing with pos & neg index
    This program shows how to slice strings in Python using both positive and negative indexing. It also demonstrates how to use slicing with the len() function and reverse slicing using step -1. Great for learning string manipulation techniques.

---
+++ string_slicing_complete_demo.py
@@ -0,0 +1,15 @@
+# 1) :-    
+
+str = "Mohit Sharma"
+print (str[1:3])
+
+#2):-
+
+str = "Hello Guyss"
+print(str[:4]) #[0:4]
+print(str[5:]) #[5:len(str)]
+
+# Slicing / by negative index
+
+str = "Mohit Sharma"
+print(str[-3: -1])
