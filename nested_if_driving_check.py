0286bbd162175e80aa69169d74186f67d53770fd
mohittsharma07 <mohittt247@gmail.com>
2025-07-02 22:08:19 +0530

    fixed nested if logic for driving eligibility
    This Python program checks if a person is allowed to drive
    using nested if-else structure. It considers age between
    18 and 80 (inclusive) as eligible for driving.

---
+++ nested_if_driving_check.py
@@ -0,0 +1,14 @@
+# print program for driving check :-
+
+age = int(input("Enter age: "))
+
+if(age >= 18):
+    if(age <= 80):
+        print("Can drive")
+    else:
+        print("can not drive")
+else:
+   print("Can not drive")
+
+
+
