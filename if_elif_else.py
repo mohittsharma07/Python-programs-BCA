6ed5d34be7c452081e905337c914949051685eb1
mohittsharma07 <mohittt247@gmail.com>
2025-07-02 21:06:09 +0530

    Add traffic light signal logic using if-elif-else
    -This Python script simulates traffic light behavior using an if-elif-else statement.
    -It prints "stop", "go", or "look" based on the value of the 'light' variable.

---
+++ if_elif_else.py
@@ -0,0 +1,13 @@
+light = "red"
+
+if(light == "red"):
+    print("stop")
+
+elif(light == "green"):
+    print("go")
+
+elif(light =="yellow"):
+    print("look")
+
+else:
+    print("light is broken")
