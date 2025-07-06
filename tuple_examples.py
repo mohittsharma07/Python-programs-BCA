aaea4ef5dce04f685cde6c08c0683184db206730
mohittsharma07 <mohittt247@gmail.com>
2025-07-06 22:41:00 +0530

    Add examples of tuple creation
    This program demonstrates how to create different types of tuples in Python, including multi-element, single-element, and string element tuples.

---
+++ tuple_examples.py
@@ -0,0 +1,15 @@
+# Normal tuple...
+tup = (1,2,3,4,5)
+print(type(tup))
+print(tup)
+
+# Single tuple...
+tup = (1,)
+print(type(tup))
+print(tup)
+
+# String element tuple...
+tup = ("Hello",)
+print(type(tup))
+print(tup)
+
