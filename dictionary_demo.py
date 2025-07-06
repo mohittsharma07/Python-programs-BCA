2f9da794367c4a471e46848e1955423395628f45
mohittsharma07 <mohittt247@gmail.com>
2025-07-07 02:22:59 +0530

    Add dictionary demo program
    This Python program demonstrates how to use dictionaries to store and update key-value pairs. It shows:
    - Creating a dictionary
    - Accessing values using keys
    - Updating an existing key
    - Adding a new key-value pair

---
+++ dictionary_demo.py
@@ -0,0 +1,16 @@
+info = {
+    "name" : "Mohit sharma",
+    "age"  : 19,
+    "is adult" : True,
+    "marks"  : 94.4,
+    "learning" : "coding",
+}
+
+print(info["name"])
+print(info["age"])
+print(info["is adult"])
+print(info["marks"])
+
+info["name "] = "Hello"
+info["Hello"] = "World"
+print(info)
