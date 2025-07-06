a2ce19266981fb84ada73860fbe176dcab172fad
mohittsharma07 <mohittt247@gmail.com>
2025-07-06 21:58:15 +0530

    Add examples for common list methods
    This file contains demonstration of Python list methods including append(), sort(), reverse(), insert(), remove(), and pop().

---
+++ list_methods_demo.py
@@ -0,0 +1,40 @@
+# 1) list.append(9)....
+list = [3,7,5,2,1]
+list.append(9)
+print(list)
+
+# 2) list.sort() in ascending order...
+list = [3,7,5,2,1,4,6,8,9]
+list.sort()
+print(list)
+
+
+# 3) list.sort(reverse = True) in descending order...
+list =  [3,7,5,2,1,4,6,8,9]
+list.sort(reverse = True)
+print(list)
+
+
+
+# 3) list.reverse()...
+list =  [3,7,5,2,1,4,6,8,9]
+list.reverse()
+print(list)
+
+
+# 4) list.insert(idx,el)...
+list = [2,1,3]
+list.insert(1,5)
+print(list)
+
+
+# 5) list.remove()...
+list = [2,3,6,8,9,7]
+list.remove(8)
+print(list)
+
+
+# 6) list.pop(idx)...
+list = [4,5,3,6]
+list.pop(3)
+print(list)
