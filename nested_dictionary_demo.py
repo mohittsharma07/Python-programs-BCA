9e4c33c73d02acaa6626974038ff6f2bbd88eed1
mohittsharma07 <mohittt247@gmail.com>
2025-07-07 02:40:40 +0530

    Add nested dictionary example
    This Python program demonstrates the use of nested dictionaries. 
    It stores a student's name and their subject-wise marks in a structured format, 
    and shows how to access inner dictionary values using multiple keys.

---
+++ nested_dictionary_demo.py
@@ -0,0 +1,10 @@
+student = {
+    "name" : "Mohit sharma",
+    "subjects" : {
+        "phy" : 87,
+        "chem" : 98,
+        "maths" : 90
+    }
+}
+print(student)
+print(student["subjects"]["maths"])
