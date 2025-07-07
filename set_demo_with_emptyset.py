87c225ef3bce76d106120987d1ef9974cfb156cc
mohittsharma07 <mohittt247@gmail.com>
2025-07-07 16:54:33 +0530

    Add python set and empty set demo
    This Python script demonstrates how to create a set with multiple elements and shows that duplicate values are automatically removed. It also includes an example of how to define an empty set using the set() constructor.

---
+++ set_demo_with_emptyset.py
@@ -0,0 +1,12 @@
+collection = {1,2,3,4,2,"Hello","world",9,"Hello"}
+
+print(collection)
+print(len(collection)) # Total numbers of items.
+print(type(collection))
+
+
+
+# Empty set...
+collection = set() # empty set.
+
+print(type(collection))
