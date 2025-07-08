8112d5a83acdc7d4b77bede6e258b3979de3688f
mohittsharma07 <mohittt247@gmail.com>
2025-07-08 19:38:43 +0530

    Add 5 examples of python range() function
    This Python script demonstrates the use of the built-in range() function in 5 different ways:
    
    Example 1: Storing range(10) in a variable and iterating.
    
    Example 2: Direct use of range(10) in a for loop.
    
    Example 3: Printing the range object directly.
    
    Example 4: Using range(start, stop) to control start point.
    
    Example 5: Using range(start, stop, step) to control increment.
    
    
    Helpful for understanding how loops work with range() in Python.

---
+++ range_function_examples.py
@@ -0,0 +1,20 @@
+print("Example 1:Storing range(10) in variable")
+seq = range(10) # range (stop)
+for i in seq:
+    print(i)
+
+print("\nExample 2: range(10)")
+for i in range(10): # range(stop)
+    print(i)
+   
+print("\nExample 3: print range object directly")
+print(range(10)) # range(stop)
+
+print("\nExample 4: range(2,10)")
+for i in range(2,10): # range(start,stop)
+    print(i)
+
+print("\nExample 5: range(2,10,2)")
+for i in range(2,10,2): # raange(start,stop,step)
+    print(i)
+
