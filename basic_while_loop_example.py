5d8432ea5360d280c6e917fea7d671a26d3c0716
mohittsharma07 <mohittt247@gmail.com>
2025-07-08 17:28:13 +0530

    Add basic while loop programs with input and print
    This file contains multiple basic while loop programs:
    1. Print a name 10 times.
    2. Print numbers from 1 to 10.
    3. Print numbers from 5 to 1 (reverse).
    4. Take user input and print multiplication table.

---
+++ basic_while_loop_example.py
@@ -0,0 +1,27 @@
+# 1) Print name from 10 times...
+i = 1
+while i <= 10:
+    print("Mohit sharma")
+    i += 1
+
+
+# 2) Print numbers from 1 to 10...
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+
+
+# 3) Print numbers from 5 to 1...
+i = 5
+while i >= 1:
+    print(i)
+    i -= 1
+
+
+# 4) Print the multiplication table of a numbers n...
+n = int(input("Enter number"))
+i = 1
+while i <= 10:
+    print(n*i)
+    i += 1
