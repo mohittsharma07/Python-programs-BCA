d07f8880411a2e2e7bb5067870b7716e21a0af3a
mohittsharma07 <mohittt247@gmail.com>
2025-07-08 18:53:37 +0530

    Add various for loop examples with else
    This Python script demonstrates multiple use cases of the for loop:
    
    Example 1: Looping through a list using for.
    
    Example 2: Looping through a tuple.
    
    Example 3: Iterating over characters in a string.
    
    Example 4: Printing elements from a list of numbers.
    
    Example 5: Using for-else with a break to find a specific character in a string.

---
+++ for_loop_examples.py
@@ -0,0 +1,52 @@
+# 1) 
+nums = [1,2,3,4,5,6,7]
+
+for val in nums:
+    print(val)
+
+print("End 1st exaple\n")
+
+
+
+# 2)
+tup = (1,2,4,5,7,8,9)
+
+for num in tup:
+    print(num)
+
+print("End 2nd exaple\n")
+
+
+
+# 3)
+str = "Mohit sharma"
+
+for char in str:
+    print(char)
+
+print("End 3rd exaple\n")
+
+
+
+# 4) Print the elements of the following list using a loop..[1,4,9,16,25,36,49,64,81,100]
+nums = [1,4,9,16,25,36,49,64,81,100]
+
+for el in nums :
+    print(el)
+
+print("End 4th exaple\n")
+
+
+
+# 5) for loop wirth else...
+str = "Mohit sharma"
+
+for char in str:
+    if(char == 'o'):
+        print("a found")
+        break
+    print(char)
+else:
+    print("End")
+
+print("End 5th exaple\n")
