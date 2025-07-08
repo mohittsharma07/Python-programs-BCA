325793e25ae44c60f72770b62ac1cf3929b586eb
mohittsharma07 <mohittt247@gmail.com>
2025-07-08 19:47:19 +0530

    Add for loop examples using range functions
    This Python script demonstrates how to use the for loop with the range() function through 3 different examples:
    
    Example 1: Prints numbers from 1 to 50 using range(1, 51).
    
    Example 2: Prints numbers from 50 to 1 using reverse range(50, 0, -1).
    
    Example 3: Takes a number from user input and prints its multiplication table from 1 to 10.
    
    
    Useful for practicing loops, range usage, and basic input/output in Python.

---
+++ using_for_range_examples.py
@@ -0,0 +1,26 @@
+# 1) print numbers from 1 to 50...
+
+for i in range(1,51):
+    print (i)
+
+
+print("End 1st exaple\n")
+
+
+# 2) print numbers from 50 to 1...
+
+for i in range(50,0,-1):
+    print(i)
+
+print("End 2nd exaple\n")  
+
+
+
+# 3) print the multiplication table of a number n...
+
+n = int(input("Enter number"))
+
+for i in range (1,11):
+    print(n*i)
+
+print("End 3rd exaple\n")
