1ee4f0b71ef3ce22c4774835a80c40847fe08e2d
mohittsharma07 <mohittt247@gmail.com>
2025-07-09 16:43:00 +0530

    Add examples of default parameters
    This Python script demonstrates the use of default parameters in functions through two simple examples:
    
    1. cal_prod(a, b = 6) – A function to multiply two numbers, where the second number has a default value of 6.
       - Example: cal_prod(5) ➝ 5 * 6 = 30
    
    2. greet(name = "Guest") – A function that prints a greeting. If no name is provided, it uses "Guest" by default.
       - Example: greet("Mohit") ➝ Hello Mohit
       - Example: greet() ➝ Hello Guest
    
    These examples are great for beginners to understand default function arguments in Python.

---
+++ function_with_default_param_example.py
@@ -0,0 +1,20 @@
+# 1) Print number for multiplication...
+
+def cal_prod(a,b = 6):
+    print(a * b)
+    return a * b
+
+cal_prod(5)
+
+print("END 1st example\n")
+
+
+# 2) Print name...
+
+def greet(name = "Guest"):
+    print("Hello",name)
+
+greet("Mohit")
+greet()
+
+print("END 2ndample\n")
